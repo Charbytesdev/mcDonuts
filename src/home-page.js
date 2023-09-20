5db2847c0e2b35c87339f9ec8b0df5ce847d280f
Charbytesdev <138222011+Charbytesdev@users.noreply.github.com>
2023-09-20 23:42:56 +0300

    Add basic nav style for home-page

--- src/home-page.js
+++ src/home-page.js
@@ -1,7 +1,9 @@
 import BGImageSource from "./bg-image.jpg";
+import "./css/style.css";
 
 export default function () {
   const nav = document.createElement("nav");
+  const navButtonContainer = document.createElement("div");
   const main = document.createElement("main");
 
   const restaurantName = document.createElement("h1");
@@ -9,20 +11,27 @@ export default function () {
   const menuButton = document.createElement("button");
   const aboutButton = document.createElement("button");
   const bgImage = document.createElement("img");
+  const divHelper = document.createElement("div");
 
   nav.classList.add("nav-bar");
   restaurantName.classList.add("restaurant-name");
-  homeButton.classList.add("home-btn");
-  menuButton.classList.add("menu-btn");
-  aboutButton.classList.add("about-btn");
+  homeButton.classList.add("home-btn", "nav-btn");
+  menuButton.classList.add("menu-btn", "nav-btn");
+  aboutButton.classList.add("about-btn", "nav-btn");
+  navButtonContainer.classList.add("nav-btn-container");
 
   bgImage.src = BGImageSource;
   restaurantName.textContent = "mcDonuts";
+  homeButton.textContent = "HOME";
+  menuButton.textContent = "MENU";
+  aboutButton.textContent = "ABOUT";
 
   nav.appendChild(restaurantName);
-  nav.appendChild(homeButton);
-  nav.appendChild(menuButton);
-  nav.appendChild(aboutButton);
+  navButtonContainer.appendChild(homeButton);
+  navButtonContainer.appendChild(menuButton);
+  navButtonContainer.appendChild(aboutButton);
+  nav.appendChild(navButtonContainer);
+  nav.appendChild(divHelper);
   main.appendChild(bgImage);
 
   document.body.appendChild(nav);
