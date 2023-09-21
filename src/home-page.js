f3c0d442f65f820b8e4506b6da7a75dbfa65adf3
Charbytesdev <138222011+Charbytesdev@users.noreply.github.com>
2023-09-21 18:35:04 +0300

    Add hero and impelement a basic style

--- src/home-page.js
+++ src/home-page.js
@@ -5,7 +5,6 @@ export default function () {
 
   const nav = document.createElement("nav");
   const navButtonContainer = document.createElement("div");
-  const main = document.createElement("main");
   const hero = document.createElement("div");
 
   const restaurantName = document.createElement("h1");
@@ -13,18 +12,27 @@ export default function () {
   const menuButton = document.createElement("button");
   const aboutButton = document.createElement("button");
 
+  const heroText = document.createElement("div");
+  const heroButton = document.createElement("button");
+
   homePage.classList.add("home-page");
   nav.classList.add("nav-bar");
+  hero.classList.add("hero");
   restaurantName.classList.add("restaurant-name");
   homeButton.classList.add("home-btn", "nav-btn");
   menuButton.classList.add("menu-btn", "nav-btn");
   aboutButton.classList.add("about-btn", "nav-btn");
   navButtonContainer.classList.add("nav-btn-container");
+  heroText.classList.add("hero-text");
+  heroButton.classList.add("hero-btn");
 
   restaurantName.textContent = "mcDonuts";
   homeButton.textContent = "HOME";
   menuButton.textContent = "MENU";
   aboutButton.textContent = "ABOUT";
+
+  heroText.textContent = "Hungry for McDonuts?";
+  heroButton.textContent = "View Menu";
 
   nav.appendChild(restaurantName);
   navButtonContainer.appendChild(homeButton);
@@ -32,8 +40,10 @@ export default function () {
   navButtonContainer.appendChild(aboutButton);
   nav.appendChild(navButtonContainer);
 
+  hero.appendChild(heroText);
+  hero.appendChild(heroButton);
+
   homePage.appendChild(nav);
-  homePage.appendChild(main);
   homePage.appendChild(hero);
   document.body.appendChild(homePage);
 }
