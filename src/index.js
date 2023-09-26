3a08cde4102dbfdb71e9ec81eaa5ff0d23c21657
Charbytesdev <138222011+Charbytesdev@users.noreply.github.com>
2023-09-26 11:12:46 +0300

    Refactor code and add nav button functionality

--- src/index.js
+++ src/index.js
@@ -7,13 +7,15 @@ import navBar from "./nav-bar";
 // const menuButton = navButtons.childNodes[1];
 // const aboutButton = navButtons.childNodes[2];
 
-const bodyDisplay = (() => {
+const body = (() => {
   let _currentPage;
   let _pageList = [];
 
   const setCurrentPage = (currentPage) => {
     _currentPage = currentPage;
   };
+
+  const create = (node) => node.create();
 
   const append = (...nodes) => {
     nodes.forEach((node) => {
@@ -26,18 +28,28 @@ const bodyDisplay = (() => {
   };
 
   const swapToPage = (page) => {
-    _currentPage.style.display = "none";
-    let pageNode = page.create();
-    setCurrentPage(pageNode);
-    append(pageNode);
+    if (_currentPage != page && _currentPage) {
+      _currentPage.style.display = "none";
+    }
+    append(page);
+    setCurrentPage(page);
   };
 
-  const _initialize = (() => {
-    append(navBar.create());
-    createPages(homePage);
-    setCurrentPage(_pageList[0]);
-    swapToPage(homePage);
-  })();
+  const initializeButtons = (navBar) => {
+    let navButtons = [...navBar.children[1].children];
+    navButtons.forEach(
+      (button) =>
+        (button.onclick = () => {
+          swapToPage(_pageList[navButtons.indexOf(button)]);
+        })
+    );
+  };
 
-  return { append, swapToPage };
+  const initialize = (() => {
+    let navBarNode = create(navBar);
+    append(navBarNode);
+    initializeButtons(navBarNode);
+    createPages(homePage);
+    swapToPage(_pageList[0]);
+  })();
 })();
